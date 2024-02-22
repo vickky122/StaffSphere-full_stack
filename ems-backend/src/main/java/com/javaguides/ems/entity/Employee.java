48be6fb8d2449bb4243624480a2f5c8f216bd01e
Vikrant Yadav <vikrantmani45@gmail.com>
2024-02-22 23:05:13 +0530

    Update Employee.java

--- ems-backend/src/main/java/com/javaguides/ems/entity/Employee.java
+++ ems-backend/src/main/java/com/javaguides/ems/entity/Employee.java
@@ -23,3 +23,16 @@ public class Employee {
     @Column(name = "email_id",nullable = false, unique = true)
     private String email;
 }
+
+
+// public class Employee {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     @Column(name = "first-name")
+//     private String firstName;
+//     @Column(name = "lastName")
+//     private String lastName;
+//     @Column(name = "email_id",nullable = false, unique = true)
+//     private String email;
+// }

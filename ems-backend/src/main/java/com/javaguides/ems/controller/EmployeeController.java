b93bd5057cd787615353617005faa5979590bfba
Vikrant Yadav <vikrantmani45@gmail.com>
2024-02-22 23:08:18 +0530

    Update EmployeeController.java

--- ems-backend/src/main/java/com/javaguides/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/javaguides/ems/controller/EmployeeController.java
@@ -62,3 +62,72 @@ public class EmployeeController {
     //     return ResponseEntity.ok("Employee deleted successfully");
     // }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class EmployeeController {
+//     private EmployeeService employeeService;
+
+//     // building add employee REST API
+//     @PostMapping
+//     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+//         EmployeeDto savedEMployee = employeeService.createEmployee(employeeDto);
+//         return new ResponseEntity<>(savedEMployee, HttpStatus.CREATED);
+//     }
+
+//     // building GEt Employee REST API
+//     @GetMapping("{id}")
+//     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) { // here id and employeeId
+//                                                                                               // names are different
+//                                                                                               // that is why we need to
+//                                                                                               // give path to it
+//         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+//         return ResponseEntity.ok(employeeDto);
+//     }
+
+//     // bbuid get all employees REST API
+//     @GetMapping
+//     public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+//         List<EmployeeDto> employees = employeeService.getAllEmployees();
+//         return ResponseEntity.ok(employees);
+//     }
+
+//     // build update employee REST API
+//     @PutMapping("{id}")
+//     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,
+//             @RequestBody EmployeeDto updatedEmployee) {
+//         EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+//         return ResponseEntity.ok(employeeDto);
+//     }
+
+//     // build delete rest api
+//     @DeleteMapping("{id}")
+//     public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+//         employeeService.deleteEmployee(employeeId);
+//         return ResponseEntity.ok("Employee deleted successfully");
+//     }
+
+//     // // build delete rest api
+//     // @DeleteMapping("{id}")
+//     // public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+//     //     employeeService.deleteEmployee(employeeId);
+//     //     return ResponseEntity.ok("Employee deleted successfully");
+//     // }
+// }
